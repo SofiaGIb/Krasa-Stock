a53519504b6c35bf7b6454fa73b74c86f9199732
Sofia Ibañez <112526595+SofiaGIb@users.noreply.github.com>
2024-03-20 20:03:19 -0300

    form validation

--- KRASA STOCK/src/routes/components/AgregarVentas/AddVentas.jsx
+++ KRASA STOCK/src/routes/components/AgregarVentas/AddVentas.jsx
@@ -2,26 +2,56 @@ import { useState } from "react";
 import { addSale } from "../../../Redux/actions/actionsFunction/actions";
 import { useDispatch, useSelector } from "react-redux";
 import "./AddVentas.css";
+import * as Yup from "yup";
 import Swal from "sweetalert2";
 
 export const AddVentas = () => {
   const dispatch = useDispatch();
+  const products = useSelector((state) => state.allProducts);
   const sale = useSelector((state) => state.sales.sales);
   const [productName, setProductName] = useState("");
   const [total, setTotal] = useState(0);
+  const [errors, setErrors] = useState({});
+
+  const schema = Yup.object().shape({
+    productName: Yup.string().required("El nombre es requerido"),
+    total: Yup.number()
+      .required("La cantidad es requerida")
+      .positive("La cantidad debe ser positiva"),
+  });
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const totalvalue = parseInt(total);
-    const newSale = { productName, total: totalvalue };
-    await dispatch(addSale(newSale));
-    Swal.fire({
-      position: "center",
-      icon: "success",
-      title: "venta registrada",
-      showConfirmButton: false,
-      timer: 1800, // 1.5 segundos
-    });
+
+    try {
+      await schema.validate({ productName, total }, { abortEarly: false });
+      const productExists = products.some((product) => product.name === productName);
+      if (!productExists) {
+        return Swal.fire({
+          position: "center",
+          icon: "error",
+          title: "PRODUCTO INGRESADO NO EXISTE",
+          showConfirmButton: false,
+          timer: 1800,
+        });
+      }
+      const newSale = { productName, total:parseInt(total) };
+      await dispatch(addSale(newSale));
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "venta registrada",
+        showConfirmButton: false,
+        timer: 1800,
+      });
+      setErrors({});
+    } catch (validationErrors) {
+      const errors = {};
+      validationErrors.inner.forEach((error) => {
+        errors[error.path] = error.message;
+      });
+      setErrors(errors);
+    }
   };
 
   return (
@@ -34,12 +64,16 @@ export const AddVentas = () => {
           value={productName}
           onChange={(event) => setProductName(event.target.value)}
         />
+        {errors.productName && <div>{errors.productName}</div>}
+
         <input
           type="number"
           placeholder="total"
           value={total}
           onChange={(event) => setTotal(event.target.value)}
         />
+        {errors.total && <div>{errors.total}</div>}
+
         <button type="submit" className="buttonv">
           CREAR
         </button>
--- KRASA STOCK/src/routes/components/stock/Stock.jsx
+++ KRASA STOCK/src/routes/components/stock/Stock.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { newStock } from "../../../Redux/actions/actionsFunction/actions";
 import { useDispatch, useSelector } from "react-redux";
 import "./stock.css";
-import SearchBar from "../searchBar/Searchbar";
+import Swal from "sweetalert2";
 import * as Yup from "yup";
 
 export default function Stock() {
@@ -22,9 +22,27 @@ export default function Stock() {
     event.preventDefault();
     try {
       await schema.validate({ name, amount }, { abortEarly: false });
+      const productExists = products.some((product) => product.name === name);
+      if (!productExists) {
+        return Swal.fire({
+          position: "center",
+          icon: "error",
+          title: "PRODUCTO INGRESADO NO EXISTE",
+          showConfirmButton: false,
+          timer: 1800,
+        });
+      }
       const newAmount = { name: name, amount: parseInt(amount) };
-
+  
+  
       await dispatch(newStock(newAmount));
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "STOCK NUEVO REGISTRADO",
+        showConfirmButton: false,
+        timer: 1800, // 1.5 segundos
+      });
       setErrors({});
     } catch (validationErrors) {
       const errors = {};
@@ -32,36 +50,36 @@ export default function Stock() {
         errors[error.path] = error.message;
       });
       setErrors(errors);
-    } }
-
-    return (
-      <div className="cajast">
-        <h2 className="tittleS">ACTUALIZAR STOCK</h2>
-
-        <form className="forms" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="nombre producto"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-          />
-          {errors.name && <div>{errors.name}</div>}
-
-          <input
-            type="number"
-            placeholder="nuevo stock"
-            value={amount}
-            onChange={(event) => {
-              setAmount(event.target.value);
-            }}
-          />
-
-          {errors.amount && <div>{errors.amount}</div>}
-          <button type="submit" className="bottons">
-            AGREGAR
-          </button>
-        </form>
-      </div>
-    );
+    }
   };
 
+  return (
+    <div className="cajast">
+      <h2 className="tittleS">ACTUALIZAR STOCK</h2>
+
+      <form className="forms" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="nombre producto"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        {errors.name && <div>{errors.name}</div>}
+
+        <input
+          type="number"
+          placeholder="nuevo stock"
+          value={amount}
+          onChange={(event) => {
+            setAmount(event.target.value);
+          }}
+        />
+
+        {errors.amount && <div>{errors.amount}</div>}
+        <button type="submit" className="bottons">
+          AGREGAR
+        </button>
+      </form>
+    </div>
+  );
+}
