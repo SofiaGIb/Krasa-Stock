8e92086e86e3c25332b9ef1c45adc49eb865bc55
Sofia Ibañez <112526595+SofiaGIb@users.noreply.github.com>
2024-03-30 18:21:52 -0300

    route

--- KRASA STOCK/src/App.jsx
+++ KRASA STOCK/src/App.jsx
@@ -6,8 +6,10 @@ import Stock from "./routes/components/stock/Stock";
 import Ventas from "./routes/components/ventas/Ventas";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { AddProduct } from "./routes/components/agregarProducto/AddProduct";
-import {AddVentas} from "./routes/components/agregarVentas/AddVentas";
 import Product from "./routes/components/productos/Productos"
+import { AddVentas } from "./routes/components/AgregarVentas/AddVentas";
+
+
 import axios from "axios";
 axios.defaults.baseURL="http://localhost:3001/"
 function App() {
