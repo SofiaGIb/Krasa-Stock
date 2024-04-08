f138d5fe7e70a93b8b91678c0e315321d75f5c40
Sofia Ibañez <112526595+SofiaGIb@users.noreply.github.com>
2024-04-08 12:06:48 -0300

    entenr

--- KRASA STOCK/vite.config.js
+++ KRASA STOCK/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { entry: "krasa stock/KRASA STOCK/server/index.js" },
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: 'src/index.js'
-    }
-  }
-
-})
+      input: "src/index.js",
+    },
+  },
+});
